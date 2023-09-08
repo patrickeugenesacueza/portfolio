a5a205b03552224510f770210f6b0724be245361
patrickeugenesacueza <127990921+patrickeugenesacueza@users.noreply.github.com>
2023-09-09 01:46:38 +0800

    image-problem-fixed

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -1,4 +1,4 @@
-import Data from '../../data/index.json';
+import data from '../../data/index.json';
 
 export default function MySkills() {
     return (
@@ -9,11 +9,15 @@ export default function MySkills() {
             </div>
 
             <div className="skills--section--container">
-                {Data?.skills?.map((item, index) => (
+                {data?.skills?.map((item, index) => (
                     <div key={index}
                         className="skills--section--card">
                         <div className="skills--section--img">
-                            <img src={item.src} alt="Product Chain" />
+                            <img src={item.src} width={54} height={54} alt="Product Chain" />
+                        </div>
+                        <div className="skills--section--card--content">
+                            <h3 className="skills--section--title">{item.title}</h3>
+                            <p className="skills--section--description">{item.description}</p>
                         </div>
                     </div>
                 ))}
